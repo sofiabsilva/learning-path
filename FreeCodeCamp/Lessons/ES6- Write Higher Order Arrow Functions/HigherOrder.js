37d3d81cdde2af4f3a95ccac87f3cf89c02fa9e0
Sofia Silva <36445382+sofiabsilva@users.noreply.github.com>
2018-07-02 11:55:37 +0100

    Update HigherOrder.js

--- FreeCodeCamp/Lessons/ES6- Write Higher Order Arrow Functions/HigherOrder.js
+++ FreeCodeCamp/Lessons/ES6- Write Higher Order Arrow Functions/HigherOrder.js
@@ -1,9 +1,23 @@
+// Given exercise:
+
 const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34];
 const squareList = (arr) => {
   "use strict";
   // change code below this line
-  const squaredIntegers = arr .filter((i) => Number.isInteger(i)) .filter(p => p > 0) .map(x => x*x)
+  const squaredIntegers = arr;
   // change code above this line
+  return squaredIntegers;
+};
+// test your code
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
+
+//My Solution:
+
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34];
+const squareList = (arr) => {
+  "use strict";
+  const squaredIntegers = arr .filter((i) => Number.isInteger(i)) .filter(p => p > 0) .map(x => x*x)
   return squaredIntegers;
 };
 // test your code
