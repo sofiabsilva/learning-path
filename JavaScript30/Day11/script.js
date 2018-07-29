dead23cda0d56af6ce7cabe31a8b7460b1acc04a
Sofia Silva <36445382+sofiabsilva@users.noreply.github.com>
2018-07-29 23:36:07 +0100

    updating progress bar in real time

--- JavaScript30/Day11/script.js
+++ JavaScript30/Day11/script.js
@@ -2,9 +2,11 @@
 /*
  - play video on clicking the start button ✔️
  - change volume when sliding the volume slider (1st one);  ✔️
- - change the speed of the video by sliding 2nd player__slider
- - using the last two buttons to go back 10s or forwards 25 s.
+ - change the speed of the video by sliding 2nd player__slider ✔️
+ - using the last two buttons to go back 10s or forwards 25 s ✔️
  - when clicking anywhere on the player window start playing/stop playing ✔️
+ - progress updates in real time ✔️
+ - using the video progress slider to change the current Time
 */
 
 // get elements:
@@ -14,6 +16,7 @@ const volume = document.querySelector('#volumeSlider');
 const playBack = document.querySelector('#playBackSlider');
 const rewind = document.querySelector('#rewind');
 const skipForward = document.querySelector('#skipForward');
+const progress = document.querySelector('.progress__filled');
 
 //initializing variables
 let playing = false;
@@ -22,15 +25,16 @@ function togglePlay() {
   playing = !playing;
   if (playing) {
     video.play();
+    playButton.innerHTML = "⏸️";
   } else {
     video.pause();
+    playButton.innerHTML = "►";
   }
 }
 
 function changeVolume() {
   video.volume = `${volume.value}`;
 }
-
 function changePlayBackRate() {
   video.playbackRate = `${playBack.value}`;
 }
@@ -40,7 +44,13 @@ function goBackTime() {
 function goForwardInTime() {
   video.currentTime = `${video.currentTime + 25}`;
 }
+// for progress bar:
+// get the flex-basis percentage: 0% when currentTime= 0, 100% at video.duration...
 
+function showProgress() {
+  const percent = (video.currentTime / video.duration) *100;
+  progress.style.flexBasis = `${percent}%`;
+}
 // Hooking event listeners
 playButton.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
@@ -50,3 +60,4 @@ playBack.addEventListener('change', changePlayBackRate);
 playBack.addEventListener('mousemove', changePlayBackRate);
 rewind.addEventListener('click', goBackTime);
 skipForward.addEventListener('click', goForwardInTime);
+video.ontimeupdate= showProgress; //to keep updating the progress bar
