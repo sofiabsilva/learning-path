0ed11c95256daaf4db626a2d2130f05e0e5ee583
Sofia Silva <36445382+sofiabsilva@users.noreply.github.com>
2018-07-29 17:05:37 +0100

    play/pause video

--- JavaScript30/Day11/script.js
+++ JavaScript30/Day11/script.js
@@ -6,3 +6,25 @@
  - using the last two buttons to go back 10s or forwards 25s;
  - when clicking anywhere on the player window start playing/stop playing;
 */
+
+// get elements:
+const playButton = document.querySelector('.player__button');
+const video = document.querySelector('.player__video');
+
+//initializing variables
+let playing = false;
+
+
+// functions
+function togglePlay() {
+  playing = !playing;
+  if (playing) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+// Hooking event listeners
+playButton.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
