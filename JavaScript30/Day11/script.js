6f06f75dbc94dfffb5e2557cc1757daca172849b
Sofia Silva <36445382+sofiabsilva@users.noreply.github.com>
2018-07-29 15:51:26 +0100

    to-do

---
+++ JavaScript30/Day11/script.js
@@ -0,0 +1,8 @@
+//TODO:
+/*
+ - play/pause video on clicking the start button;
+ - change volume when sliding the volume slider (1st one);
+ - change the speed of the video by sliding 2nd player__slider;
+ - using the last two buttons to go back 10s or forwards 25s;
+ - when clicking anywhere on the player window start playing/stop playing;
+*/
