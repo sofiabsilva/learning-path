21f8122daeb2b4a24d81d8a6cc90d5908f608cd0
Sofia Silva <36445382+sofiabsilva@users.noreply.github.com>
2018-09-25 12:19:13 +0100

    Create confirm-ending.js

---
+++ FreeCodeCamp/Basic Algorithms/confirm-ending.js
@@ -0,0 +1,13 @@
+// Check if a string (first argument, str) ends with the given target string (second argument, target).
+// This challenge can be solved with the .endsWith() method, which was introduced in ES2015.
+// But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
+
+
+function confirmEnding(str, target) {
+  let end = target + '$';
+  let lastRegex = new RegExp(end);
+  return lastRegex.test(str);
+}
+
+// testing
+confirmEnding("Bastian", "n");
