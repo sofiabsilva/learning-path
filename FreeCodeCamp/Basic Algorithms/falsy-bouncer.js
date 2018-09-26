db7a7e19a0c22ad7014c9e8f7c980a3ba6e51797
Sofia Silva <36445382+sofiabsilva@users.noreply.github.com>
2018-09-26 18:07:02 +0100

    Create falsy-bouncer.js

---
+++ FreeCodeCamp/Basic Algorithms/falsy-bouncer.js
@@ -0,0 +1,22 @@
+// Remove all falsy values from an array.
+// Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+// Hint: Try converting each value to a Boolean.
+
+function bouncer(arr) {
+ 
+  let originalArr = arr.slice();
+  // fixes index of arr if it's already been sliced
+  let fix = 0;
+
+  for (let i = 0; i < originalArr.length; i++) {
+    let bool = originalArr[i];
+    if (Boolean(bool) === false) {
+      arr.splice(i - fix, 1);
+      fix++;
+    }
+  }
+ return arr;
+}
+
+// Testing
+bouncer([7, "ate", "", false, 9]);
