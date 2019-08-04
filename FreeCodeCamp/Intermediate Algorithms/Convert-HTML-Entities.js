5ca9b1314484b97e0505ab50a63c761a7253c608
Sofia Silva <36445382+sofiabsilva@users.noreply.github.com>
2019-08-04 19:55:44 +0100

    Update Convert-HTML-Entities.js

--- FreeCodeCamp/Intermediate Algorithms/Convert-HTML-Entities.js
+++ FreeCodeCamp/Intermediate Algorithms/Convert-HTML-Entities.js
@@ -1,3 +1,7 @@
+/*
+Convert the characters &, <, >, "(double quote), and '(apostrophe), in a string to their corresponding HTML entities.
+*/
+
 function convertHTML(str) {
 
   var newStr = str.replace(/&/g, '&amp;')
